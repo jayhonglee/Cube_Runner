7bc364e6ca2f6f7a693e16115b300cb48a8b8eff
Jay <ja34luv@gmail.com>
2020-09-24 10:36:43 -0700

    First Commit
    Added Unity Project to Github

---
+++ Cube_Runner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody Rb;
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 500f;
+
+    void FixedUpdate()
+    {
+        Rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            Rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(Rb.position.y < 0.5f)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }    
+
+    }
+}
